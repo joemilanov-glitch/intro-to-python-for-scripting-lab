b9f79f4f064d5cecdd59dac126e8821c08268eea
Joemilanov <joemilanov@gmail.com>
2026-03-10 18:15:20 -0400

    commit message

---
+++ hello_python.py
@@ -0,0 +1,6 @@
+# name = input("What is your name?: ")
+# # f strings are used to inject values from other variables into strings
+# # we also calls these template strings!
+
+# greeting = f"Hello, {name}"
+# # print(greeting)
